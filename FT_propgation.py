f6b366d582486131d1eff0d8495ff5050da9796d
Erkang Wang <erkangwang@yahoo.com>
2024-04-22 21:38:31 -0700

    Add files via upload

--- FT_propgation.py
+++ FT_propgation.py
@@ -4,20 +4,22 @@ import numpy as np
 #L1, source plane side length
 #lamda: illumination wavelength
 #f: focal length
-def FT_prop(u1, L1, lamda, f):
-    M = u1.shape[0]
-    N = u1.shape[1]
+def FT_prop(u, L, lamda, f):
+    M = u.shape[0]
+    N = u.shape[1]
     # source sample interval
-    dx1 = L1 / (M - 1)
-    dy1 = L1 / (N - 1)
+    dx = L / (M - 1)
+    dy = L / (N - 1)
     k = 2 * np.pi / lamda
     # define observation plane size and sample intervals
-    L2 = lamda * f / dx1
-    x2 = np.linspace(-L2/2, L2/2, M)
-    y2 = np.linspace(-L2/2, L2/2, N)
-    Xi, Yi = np.meshgrid(x2, y2)
-    Chirp_Phase = 1 / (1j * lamda * f) * np.exp(1j * k / (2*f) * (Xi**2 + Yi**2))
-    #u2 = Chirp_Phase * np.fft.fft2(np.fft.fftshift(u1)) * dx1 * dy1
-    u2 = np.fft.fft2(u1)
-    u2 = np.fft.fftshift(u2) * dx1 * dy1 * Chirp_Phase
-    return u2, x2, y2, L2
+    Lout = lamda * f / dx
+    xout = np.linspace(-Lout/2, Lout/2, M)
+    yout = np.linspace(-Lout/2, Lout/2, N)
+    #Xi, Yi = np.meshgrid(xout, yout)
+    #Chirp_Phase = 1 / (1j * lamda * f) * np.exp(1j * k / (2*f) * (Xi**2 + Yi**2))
+    Chirp_Phase = 1
+
+    uout = np.fft.fftshift(u)
+    uout = np.fft.fft2(uout)
+    uout = np.fft.ifftshift(uout) * dx * dy * Chirp_Phase  #np.fft.fft2(uout) * dx * dy * Chirp_Phase
+    return uout, xout, yout, Lout
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import numpy as np
+import random
+import time
 import FT_propgation as FTP
 import matplotlib.pyplot as plt
-import random
+
 from photutils.aperture import CircularAperture
 
 def print_hi(name):
@@ -27,7 +29,7 @@ dy = y[1]-y[0]
 Xo, Yo = np.meshgrid(x, y)
 
 # on axis plan wave illuminate an aperture
-R=80
+R=20
 circ_aper = np.sqrt(Xo**2 + Yo**2) <= R
 u1 = circ_aper
 out, x2, y2, L2 = FTP.FT_prop(u1, L1, lamda, f)
@@ -92,3 +94,65 @@ image = plt.imshow(np.abs(out), extent=[-L2/2,L2/2,-L2/2,L2/2])
 plt.xlabel('x2 um')
 plt.ylabel('y2 um')
 plt.show()
+
+
+R=5
+circ_aper = np.sqrt(Xo**2 + Yo**2) <= R
+u1 = circ_aper
+
+u2, x2, y2, L2 = FTP.FT_prop(u1, L1, lamda, f)
+X2, Y2 = np.meshgrid(x2, y2)
+aper_stop = np.sqrt(X2**2 + Y2**2) <= 50
+
+u3, x3, y3, L3 = FTP.FT_prop( (u2*aper_stop), L2, lamda, f)
+
+plt.subplot(131)
+image = plt.imshow(np.real(u1), extent=[-L1/2,L1/2,-L1/2,L1/2])
+plt.xlabel('x1 um')
+plt.ylabel('y1 um')
+plt.subplot(132)
+image = plt.imshow(np.abs(u2*aper_stop), extent=[-L2/2,L2/2,-L2/2,L2/2])
+plt.xlabel('x2 um')
+plt.ylabel('y2 um')
+plt.subplot(133)
+image = plt.imshow(np.abs(u3), extent=[-L3/2,L3/2,-L3/2,L3/2])
+plt.xlabel('x3 um')
+plt.ylabel('y3 um')
+plt.show()
+
+#plt.ion()
+## initialize figure and axis for two images side by side
+#fig, (ax1, ax2) = plt.subplots(1, 2)
+#u1_initial = np.abs(u1)
+#image1 = ax1.imshow(u1_initial, extent=[-L1/2, L1/2, -L1/2, L1/2])
+#plt.xlabel('x1 um')
+#plt.ylabel('y1 um')
+#img_initial = np.abs(out)
+#image2 = ax2.imshow(img_initial, extent=[-L2/2, L2/2, -L2/2, L2/2])
+#plt.xlabel('x2 um')
+#plt.ylabel('y2 um')
+### setting title
+##plt.title("2D FDTD", fontsize=20)
+### setting x-axis label and y-axis label
+
+
+
+#for ii in range(10):
+#    Xi = 1 / 60 * ii
+#    u1 = np.exp(1j * 2 * np.pi * Xo / 10) * np.exp(1j * 2 * np.pi * Xi * Xo) * np.exp(1j * 2 * np.pi * Eta * Yo)
+#    out, x2, y2, L2 = FTP.FT_prop(u1, L1, lamda, f)
+#    new_obj = np.real(u1)
+#    new_image = np.abs(out)
+#
+#    image1.set_data(new_obj)
+#    image2.set_data(new_image)
+#    fig.canvas.draw()
+#    # This will run the GUI event
+#    # loop until all UI events
+#    # currently waiting have been processed
+#    fig.canvas.flush_events()
+#    time.sleep(0.01)
+
+
+
+
